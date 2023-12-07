1d13cd20c31a8e2039557a83d2a5a440f42103ba
Kent Tong <kent.tong.mo@gmail.com>
2023-12-07 16:28:32 +0800

    update

--- vocabassistant3/tests/test_word_meaning.py
+++ vocabassistant3/tests/test_word_meaning.py
@@ -3,16 +3,8 @@ from vocabassistant3.models import *
 
 class TestWordAndMeaning(TestCase):
     def test_display(self):
-<<<<<<< HEAD
         wam=WordAndMeaning("hand")
         wam.add_meaning("n", "手")
         wam.add_meaning("v", "遞給")
         self.assertEquals(wam.get_display(), "hand\t手(n)、遞給(v)")
       
-=======
-        wam=WordAndMeaning(0, "can")
-        wam.add_meaning("n", "罐")
-        wam.add_meaning("m", "能夠")
-        self.assertEquals(wam.get_display(), "can\t罐(n)、能夠(m)")
-   
->>>>>>> e037aa5b74b69597642bd800cdc43dc69146f3b2
