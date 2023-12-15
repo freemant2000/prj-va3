af7014f34b306234b451592f18aa6021ad880fc6
Kent Tong <kent.tong.mo@gmail.com>
2023-12-15 18:53:51 +0800

    show practices

--- vocabassistant3/tests/manual_test_practice.py
+++ vocabassistant3/tests/manual_test_practice.py
@@ -1,5 +1,8 @@
-from vocabassistant3.practice import add_practice
+from vocabassistant3.practice import add_practice, get_student, show_student
 from vocabassistant3.db_base import open_session
 
 with open_session() as s:
-  add_practice(s, 0, 2, 5)
+  #add_practice(s, 0, 2, 5)
+  stud=get_student(s, 0)
+  show_student(stud)
+
