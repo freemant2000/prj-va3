565845ed546bcc7ad3fcfeef028b6c9823f64c97
Kent Tong <kent.tong.mo@gmail.com>
2023-12-18 21:09:02 +0800

    show exercise in html

--- vocabassistant3/exercise_tui.py
+++ vocabassistant3/exercise_tui.py
@@ -1,6 +1,6 @@
 from .db_base import open_session
 from .console_utils import get_lines_until_empty
-from .sprint import Sprint, get_sprint, parse_exec_draft, refine_exec_draft, show_exec_draft, show_sprint
+from .sprint import Sprint, get_exec, get_sprint, parse_exec_draft, refine_exec_draft, show_exec_draft, show_sprint
 
 def refine_exec_draft_tui():
     with open_session() as s:
@@ -16,17 +16,38 @@ def refine_exec_draft_tui():
         except Exception as e:
             print(str(e))
 
-def init_exec_draft_tui():
-    with open_session() as s:
-        sp=get_sprint(s, 0)
-        show_sprint_for_new_exec(sp)
-        print("Paste a new exercise: words, ====, then sentences")
-        try:
-            lines=get_lines_until_empty()
-            ed=parse_exec_draft(lines)
-            with open_session() as s:
-                refine_exec_draft(s, sp, ed)
-                show_exec_draft(ed)
-        except Exception as e:
-            print(str(e))
+# def init_exec_draft_tui():
+#     with open_session() as s:
+#         sp=get_sprint(s, 0)
+#         show_sprint_for_new_exec(sp)
+#         print("Paste a new exercise: words, ====, then sentences")
+#         try:
+#             lines=get_lines_until_empty()
+#             ed=parse_exec_draft(lines)
+#             with open_session() as s:
+#                 refine_exec_draft(s, sp, ed)
+#                 show_exec_draft(ed)
+#         except Exception as e:
+#             print(str(e))
 
+def show_exec_html():
+    e_id=str(input("Enter exercise ID: "))
+    with open_session() as s:
+        exec=get_exec(s, e_id)
+        html="<div>"
+        html+="<h4 style='margin-bottom: 5px;'>默字</h4>"
+        html+="<div style='width: 300px;'>"
+        for ew in exec.ews:
+            ew_html=f"""
+            <div style="display: flex; width: 100%">
+            <span>{ew.wd.word}</span><span style="flex-grow: 1"></span><span>{ew.wd.get_meanings()}</span>
+            </div>
+            """
+            html+=ew_html
+        html+="</div>"
+        html+="<h4 style='margin-bottom: 5px;'>翻譯</h4>"
+        for snt in exec.snts:
+            html+=f"<div>{snt.text}</div>"
+    html+="</div>"
+    print(html)
+            
---
+++ vocabassistant3/tests/manual_test_exec_html.py
@@ -0,0 +1,3 @@
+from vocabassistant3.exercise_tui import show_exec_html
+
+show_exec_html()
