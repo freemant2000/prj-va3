2f0dfb64cea9fd0c8a6e14dafde6ca0ea0a15a42
Kent Tong <kent.tong.mo@gmail.com>
2023-12-29 16:40:03 +0800

    sprint page

---
+++ vocabassistant3/web_flask/sprint_wp.py
@@ -0,0 +1,52 @@
+from flask import request, redirect, render_template
+from datetime import date
+from sqlalchemy.orm import Session
+from ..teacher import get_teacher
+from .user_prod_flask import get_current_user
+from ..buf_print import buf_pr
+from ..db_base import open_session
+from ..sprint import get_revision_dates, get_sprint
+
+def sprint_wp(sp_id):
+    try:
+        tch_id=get_current_user()
+        if not tch_id:
+            return redirect("/login")
+        with open_session() as s:
+            tch=get_teacher(s, tch_id)
+            sp=get_sprint(s, sp_id)
+            if sp:
+                if not tch.teaches(sp.stu):
+                    raise ValueError(f"You are teaching that student")
+                buf_cnt=show_sprint_buf(s, sp)
+                return render_template("sprint_wp.html", 
+                                    ta_cnt=buf_cnt, 
+                                    rows=buf_cnt.count("\n"),
+                                    execs=sp.execs)
+            else:
+                raise ValueError(f"Sprint {sp_id} not found")
+    except Exception as e:
+        return "Error: "+str(e)
+
+def show_sprint_buf(s: Session, sp)->str:
+    buf, pr=buf_pr()
+    pr(f"Sprint {sp.id} started on {sp.start_dt}")
+    pr("Revision summary")
+    today=date.today()
+    rds=get_revision_dates(s, sp.id)
+    wd_ds={ew.wd: ds for (ew, ds) in rds.items()}
+    for idx, bw in enumerate(sp.get_bws()):
+        if bw.wd in wd_ds:
+            ds=wd_ds.get(bw.wd)
+            days=[str((today-d).days) for d in ds]
+            days=",".join(days)
+            rev_info=f"{days} day(s) ago"
+        else:
+            rev_info=""
+        fw=bw.get_full_word()
+        pr(f"{str(idx).ljust(3)} {fw.ljust(20)}\t{bw.wd.get_meanings()}\t{rev_info}")
+    pr("Sentences used")
+    for snt in (s for exec in sp.execs for s in exec.snts):
+        pr(snt.text)
+    return buf.getvalue()
+
--- vocabassistant3/web_flask/va3_web_main.py
+++ vocabassistant3/web_flask/va3_web_main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from disl import Disl
+
 from ..config_reader import get_config_parser
 from ..db_base import open_session
 from ..teacher import get_teacher_by_email
@@ -7,12 +8,13 @@ from .g_auth import get_user_email
 from .g_oauth2 import GoogleOAuth2
 from .user_prod_flask import set_current_user
 from .stud_wp import stud_main_wp
+from .sprint_wp import sprint_wp
 
 cp=get_config_parser()
 app=Flask(__name__)
 app.secret_key=cp.get("va3", "session_encrpt_key")
 app.add_url_rule("/stus/<int:stu_id>", view_func=stud_main_wp)
-
+app.add_url_rule("/sprints/<int:sp_id>", view_func=sprint_wp)
 di=Disl()
 di.add_raw_bean("client_secret_file", cp.get("va3", "client-secret-file"))
 di.add_raw_bean("redirect_uri", cp.get("va3", "redirect_uri"))
