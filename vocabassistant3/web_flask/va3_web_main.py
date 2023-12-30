2ee23729188084a8fc0ce57c5b6b4c65985c5f73
Kent Tong <kent.tong.mo@gmail.com>
2023-12-30 12:28:46 +0800

    remove fake login page

--- vocabassistant3/web_flask/va3_web_main.py
+++ vocabassistant3/web_flask/va3_web_main.py
@@ -5,7 +5,6 @@ from .exec_wp import exec_wp
 from .tch_land_wp import tch_land_wp
 from .oauth2_cb_wp import oauth2_cb
 from .login_wp import login_wp
-from .login_fake_wp import login_fake_wp
 from .main_disl import di
 
 app=Flask(__name__)
@@ -15,7 +14,6 @@ app.add_url_rule("/sprints/<int:sp_id>", view_func=sprint_wp)
 app.add_url_rule("/execs/<int:e_id>", view_func=exec_wp)
 app.add_url_rule("/teacher", view_func=tch_land_wp)
 app.add_url_rule("/login", view_func=login_wp)
-app.add_url_rule("/login_fake", view_func=login_fake_wp)
 app.add_url_rule("/oauth2_callback", view_func=oauth2_cb)
 
 @app.route("/")
