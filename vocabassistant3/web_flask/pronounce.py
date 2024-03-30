19a03d7f02d767ecacdb79d9683e2f38b6362753
Kent Tong <kent.tong.mo@gmail.com>
2024-03-30 16:43:39 +0800

    test

--- vocabassistant3/web_flask/pronounce.py
+++ vocabassistant3/web_flask/pronounce.py
@@ -1,8 +1,10 @@
+import imp
 from flask import send_file
 from ..tts_engine import launch_speak
 from ..word_def import get_word_def_by_id
 from ..db_base import open_session
 from os import path
+from time import sleep
 
 def pronounce(temp_dir, wd_id: int):
     try:
@@ -10,6 +12,7 @@ def pronounce(temp_dir, wd_id: int):
             wd=get_word_def_by_id(s, wd_id)
             p=path.join(temp_dir, f"va3-pron-{wd_id}.mp3")
             launch_speak(wd.word, p)
+            sleep(0.1) # sometimes it fails to find the newly created file
             return send_file(p, max_age=3600)
     except Exception as e:
         return "Error: "+str(e)
