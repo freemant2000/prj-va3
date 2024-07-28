72d374b64fcbb7007b28f379dbe83a592b0a30cc
Kent Tong <kent.tong.mo@gmail.com>
2024-07-28 13:41:09 +0800

    sort students

--- vocabassistant3/tui/teacher_tui.py
+++ vocabassistant3/tui/teacher_tui.py
@@ -2,6 +2,7 @@ from .user_prod_tui import get_current_user
 
 def show_students_tui():
     tch=get_current_user()
-    for stu in tch.stus:
+    stus=sorted(tch.stus, key=lambda s: s.id)
+    for stu in stus:
         print(f"{stu.id} {stu.name}")
 
