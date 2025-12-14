8c1a84abb157ad259febca3f2b200f246370ccc1
Kent Tong <kent.tong.mo@gmail.com>
2025-12-14 16:26:13 +0000

    update

--- input_files/chk_dup.py
+++ input_files/chk_dup.py
@@ -1,9 +1,11 @@
 from subprocess import call, DEVNULL
 
-f=open("vocab-doraemon-13-L2")
+f=open("vocab-doraemon-new")
 for e in f:
+    if e.startswith("\t"):
+        continue
     e=e.strip()
-    cmd="grep "+e+" vocab-doraemon-*-L2.txt"
+    cmd="grep '"+e+"' vocab-doraemon-*-L2.txt"
     p=call(cmd, shell=True, stdout=DEVNULL, stderr=DEVNULL)
     if p==1:
         print(e)
